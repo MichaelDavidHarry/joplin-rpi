92939043989e2549f34f301e4db9f3695c5a1e72
Michael Harry <michael@compilefailed.com>
2021-01-06 23:06:52 -0600

    Update build.sh to use code from Joplin tag v1.5.12

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -e
 git clone https://github.com/laurent22/joplin.git
 
 cd joplin
-git checkout v1.5.11
+git checkout v1.5.12
 
 # Build steps taken from https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=joplin&id=a3f942cecf06db5cbc0520467d4d6d4fcfb7db78
 
