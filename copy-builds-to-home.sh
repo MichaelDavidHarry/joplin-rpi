5ead3359aa3943ea80bd48ad84a4b902a393bc57
Michael Harry <michael@compilefailed.com>
2023-12-10 06:58:16 -0600

    Add script to copy built packages from chroots to home directory

---
+++ copy-builds-to-home.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp joplin32/chroot/joplin/packages/app-desktop/dist/*.AppImage ~
+cp joplin32/chroot/joplin/packages/app-desktop/dist/*.deb ~
+cp joplin64/chroot/joplin/packages/app-desktop/dist/*.AppImage ~
+cp joplin64/chroot/joplin/packages/app-desktop/dist/*.deb ~
