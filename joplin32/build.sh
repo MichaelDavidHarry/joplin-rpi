26c80784dba62255275cbb839063db458df258f1
Michael Harry <michael@compilefailed.com>
2024-08-25 09:02:59 -0500

    Update build.sh to use code from Joplin tag v3.0.15

--- joplin32/build.sh
+++ joplin32/build.sh
@@ -7,7 +7,7 @@ git clone https://github.com/laurent22/joplin.git
 cd joplin
 
 # Change this line to change the version being built.
-git checkout v3.0.14
+git checkout v3.0.15
 
 # Disable parallel cloning and set compression to minimum level to lessen memory demand
 echo 'cloneConcurrency: 1' >> .yarnrc.yml
