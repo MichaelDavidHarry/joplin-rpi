1e311f8ca596c9ff0c2a2cea1abc2a70f0682b9c
Michael Harry <michael@compilefailed.com>
2025-01-26 13:30:12 -0600

    Update build.sh to use code from Joplin tag v3.2.12. Update copy-build-to-home.sh to preserve unique names for builds for different architectures.

--- copy-builds-to-home.sh
+++ copy-builds-to-home.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-cp joplin32/chroot/joplin/packages/app-desktop/dist/*.AppImage ~
-cp joplin32/chroot/joplin/packages/app-desktop/dist/*.deb ~
-cp joplin64/chroot/joplin/packages/app-desktop/dist/*.AppImage ~
-cp joplin64/chroot/joplin/packages/app-desktop/dist/*.deb ~
+function copy_with_suffix_to_home(){
+    path=$1
+    suffix=$2
+
+    for file in "$path"; do
+        base=$(basename -- "$file")
+        extension="${base##*.}"
+        base="${base%.*}"
+        cp "$file" "$HOME/${base}${suffix}.${extension}"
+    done
+}
+
+copy_with_suffix_to_home joplin32/chroot/joplin/packages/app-desktop/dist/*.AppImage -armv7l
+copy_with_suffix_to_home joplin32/chroot/joplin/packages/app-desktop/dist/*.deb -armv7l
+
+copy_with_suffix_to_home joplin64/chroot/joplin/packages/app-desktop/dist/*.AppImage -arm64
+copy_with_suffix_to_home joplin64/chroot/joplin/packages/app-desktop/dist/*.deb -arm64
